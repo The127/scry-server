8b5635bfe6b46c6bafeb267c4a101d2eb5f20ff0
karo <karolin.kostial@gmail.com>
2025-05-16 14:20:49 +0200

    feat: route based on verbs

--- src/router.nim
+++ src/router.nim
@@ -2,11 +2,13 @@ import asyncdispatch
 
 type
   Request* = ref object
+    verb: string
     url: string
 
   RequestHandler* = proc(req: Request): Future[void] {.gcsafe, async.}
     
   Route = object
+    verb: string
     url: string
     handler: RequestHandler
     
@@ -14,8 +16,9 @@ type
     prefix: string
     routes: seq[Route]
   
-proc newRequest*(url: string): Request =
+proc newRequest*(verb: string, url: string): Request =
   Request(
+    verb: verb,
     url: url,
   )
 
@@ -25,19 +28,20 @@ proc url*(req: Request): string =
 proc newRouter*(prefix = ""): Router =
   Router(prefix: prefix, routes: @[])
 
-proc addRoute*(router: Router, route: string, handler: RequestHandler) =
+proc addRoute*(router: Router, verb: string, route: string, handler: RequestHandler) =
   let route = if router.prefix == "":
       route
     else:
       router.prefix & "/" & route
 
   router.routes.add(Route(
+    verb: verb,
     url: route,
     handler: handler,
   ))
 
 proc route*(router: Router, request: Request): Future[void] {.async.} =
   for route in router.routes:
-    if route.url == request.url:
+    if route.url == request.url and route.verb == request.verb:
       await route.handler(request)
       return
--- tests/test_router.nim
+++ tests/test_router.nim
@@ -8,12 +8,14 @@ suite "Router tests":
       var isCalled = false
       let router = newRouter("api/v1")
       router.addRoute(
+        "GET",
         "users",
         proc(req: Request): Future[void] {.async.} =
           isCalled = true,
       )
 
       let req = newRequest(
+        "GET",
         "api/v1/users",
       )
 
@@ -31,12 +33,14 @@ suite "Router tests":
       var isCalled = false
       let router = newRouter()
       router.addRoute(
+        "GET",
         "users",
         proc(req: Request): Future[void] {.async.} =
           isCalled = true,
       )
 
       let req = newRequest(
+        "GET",
         "users",
       )
 
@@ -55,18 +59,21 @@ suite "Router tests":
       let router = newRouter()
 
       router.addRoute(
+        "GET",
         "users",
         proc(req: Request): Future[void] {.async.} =
           isCalled = true,
       )
 
       router.addRoute(
+        "GET",
         "sessions",
         proc(req: Request): Future[void] {.async.} =
           return,
       )
 
       let req = newRequest(
+        "GET",
         "users",
       )
 
@@ -85,18 +92,21 @@ suite "Router tests":
       let router = newRouter()
 
       router.addRoute(
+        "GET",
         "sessions",
         proc(req: Request): Future[void] {.async.} =
           return,
       )
 
       router.addRoute(
+        "GET",
         "users",
         proc(req: Request): Future[void] {.async.} =
           isCalled = true,
       )
 
       let req = newRequest(
+        "GET",
         "users"
       )
 
@@ -105,5 +115,40 @@ suite "Router tests":
 
       # assert
       check isCalled
+
+    waitFor(doTest())
+
+  test "router routes based on verb":
+    proc doTest() {.async.} =
+      # arrange
+      var getCalled = false
+      var postCalled = false
+      let router = newRouter()
+
+      router.addRoute(
+        "GET",
+        "users",
+        proc(req: Request): Future[void] {.async.} =
+          getCalled = true,
+      )
+
+      router.addRoute(
+        "POST",
+        "users",
+        proc(req: Request): Future[void] {.async.} =
+          postCalled = true,
+      )
+
+      let req = newRequest(
+        "POST",
+        "users"
+      )
+
+      # act
+      await router.route(req)
+
+      # assert
+      check postCalled
+      check getCalled == false
 
     waitFor(doTest())
