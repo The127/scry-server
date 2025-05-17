3c207ab61215fafd3a76aadb9cad5371ea0ebafb
karo <karolin.kostial@gmail.com>
2025-05-17 10:48:20 +0200

    add dummy register endpoint

--- src/routes/auth.nim
+++ src/routes/auth.nim
@@ -1,4 +1,7 @@
-import ../router, ../server, ../request
+import asyncdispatch, ../router, ../server, ../request, httpbeast
+
+proc registerEndpoint(params: RouteParams, req: ScryRequest): Future[void] {.async} =
+  req.hbReq.send("register endpoint", Http501)
 
 proc addAuthRoutes*(router: Router[ScryRequest]) =
   return
