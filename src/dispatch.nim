baf97f1a487a7ecc55963aa761636c2eacbf20f9
karo <karolin.kostial@gmail.com>
2025-05-17 15:15:20 +0200

    feat: middlware support for dispat

--- src/dispatch.nim
+++ src/dispatch.nim
@@ -1,9 +1,25 @@
-import macros
+import macros, asyncdispatch
 
 type
   Command* = ref object of RootObj
+  Next* = proc(cmd: Command): Future[void] {.async.}
+  Middleware* = proc(cmd: Command, next: Next): Future[void] {.async.}
+
+var middlewares: seq[Middleware] = @[]
+
+proc registerMiddleware*(mw: Middleware) =
+  middlewares.add(mw)
 
 macro registerHandler*(cmdType: typedesc, handler: untyped): untyped =
   quote do:
     proc dispatch(cmd: `cmdType`): Future[void] {.async.} =
-      await `handler`(cmd)
+      var chain: Next = proc(cmd: Command): Future[void] {.async.} =
+        await `handler`(`cmdType`(cmd))
+
+      for middleware in middlewares:
+        let next = chain
+        let current = middleware
+        chain = proc(cmd: Command): Future[void] {.async.} =
+          await current(cmd, next)
+
+      await chain(cmd)
